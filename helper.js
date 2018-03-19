2954deb186734cd307aeb59d995a70226ac563c3
root <root@pts00452-vm4.persistent.co.in>
2018-03-19 10:50:04 +0000

    updated 1/03

--- helper.js
+++ helper.js
@@ -1,19 +1,20 @@
 function hideAll() {
-        console.log('hideAll');
+	console.log('hideAll');
 }
 
 function showme(clicked) {
-        console.log('showme');
-        var idVal = clicked.substring(7);
-        var all = document.getElementsByName("data");
-        var i;
-        for (i = 0; i < all.length; i++) {
-                all[i].style.display = "none";
-        }
-        var allSum = document.getElementsByName("summary");
+	console.log('showme');
+	var idVal = clicked.substring(7);
+	var all = document.getElementsByName("data");
+	var i;
+	for (i = 0; i < all.length; i++) {
+		all[i].style.display = "none";
+	}
+	var allSum = document.getElementsByName("summary");
         for (i = 0; i < allSum.length; i++) {
                 allSum[i].style.display = "none";
         }
-        document.getElementById(idVal).style.display = "block";
+ 
+	document.getElementById(idVal).style.display = "block";
 
 }
